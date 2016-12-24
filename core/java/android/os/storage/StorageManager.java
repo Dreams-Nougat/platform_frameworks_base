timduru/platform-frameworks-base

prevent MtpHost from crashing

core/java/android/os/storage/StorageManager.java
@@ -1239,7 +1239,10 @@ public class StorageManager {
             return mMountService.mountAppFuse(name);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
+        } catch (Exception e) {
+            Log.e(TAG, "TTT mountAppFuse error: "+e.getMessage());
         }
+        return null;
     }
 
     /// Consts to match the password types in cryptfs.h

Dreams-Nougat/platform_frameworks_base

Change-Id: Id5235f8727668af52188c3263bca0714f1340854
Signed-off-by: manumanfred <carlosmanuel_leemet@msn.com>

core/java/android/os/storage/StorageManager.java
@@ -1286,7 +1286,10 @@ public class StorageManager {
             return mMountService.mountAppFuse(name);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
+        } catch (Exception e) {
+            Log.d(TAG, "TTT mountAppFuse error: " + e.getMessage());
         }
+        return null;
     }
 
     /// Consts to match the password types in cryptfs.h